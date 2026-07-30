46cfceabf24cb07ae5c126fc0b589941b0e0a4cf
sekarrepku <latifangren@gmail.com>
2026-07-30 07:03:06 +0700

    feat: improve customize.sh logging steps for Magisk update installations

--- customize.sh
+++ customize.sh
@@ -6,14 +6,20 @@ ui_print "*        BFR WEBUI GO               *"
 ui_print "*   High-performance Control Panel   *"
 ui_print "*************************************"
 
+ui_print "- Checking for active WebUI processes..."
 # Force stop running versions to release file lock on updating
 killall -9 webui 2>/dev/null
 pkill -9 -f "webui" 2>/dev/null
+ui_print "- Prior instances terminated."
 
-ui_print "- Setting up module permissions..."
+ui_print "- Installing module to: $MODPATH"
+ui_print "- Setting up execution permissions..."
 set_perm "$MODPATH/service.sh" 0 0 0755
 set_perm "$MODPATH/webui" 0 0 0755 0755
 
-ui_print "- Activating WebUI service..."
+ui_print "- Launching WebUI dynamically..."
 # Start the service immediately without requiring reboot
 /data/adb/modules/bfr_webui_go/webui &
+
+ui_print "- Server started in background."
+ui_print "- Web Panel URL: http://127.0.0.1:8080 or http://[your-ip-address]:8080"
